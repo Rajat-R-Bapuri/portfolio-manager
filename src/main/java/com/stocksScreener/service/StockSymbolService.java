327f7c9929855bb930c0e8e5e11711494ceadba4
Rajat-R-Bapuri <rajat.bapuri@nyu.edu>
2020-07-23 12:33:35 -0400

    enable caching

--- src/main/java/com/stocksScreener/service/StockSymbolService.java
+++ src/main/java/com/stocksScreener/service/StockSymbolService.java
@@ -3,25 +3,28 @@ package com.stocksScreener.service;
 import com.stocksScreener.model.StockSymbol;
 import com.stocksScreener.repository.StockSymbolRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@CacheConfig(cacheNames={"StockSymbols"})
 public class StockSymbolService {
     private final StockSymbolRepository stockSymbolRepository;
 
     @Autowired
-    public StockSymbolService(StockSymbolRepository stockSymbolRepository, MongoOperations mongoOperations) {
+    public StockSymbolService(StockSymbolRepository stockSymbolRepository) {
         this.stockSymbolRepository = stockSymbolRepository;
     }
 
-    public void addSymbols(List<StockSymbol> stockSymbols){
+    public void addSymbols(List<StockSymbol> stockSymbols) {
         stockSymbolRepository.saveAll(stockSymbols);
     }
 
-    public List<StockSymbol> getSymbols(String searchKey){
+    @Cacheable
+    public List<StockSymbol> getSymbols(String searchKey) {
         return stockSymbolRepository.findAllBySymbolContaining(searchKey);
     }
 }
