32ef0d7a10066d9950028eb6552e40e11973bad9
Rajat-R-Bapuri <rajat.bapuri@nyu.edu>
2020-08-02 13:06:45 -0400

    update caching, add support for popular symbols

--- src/main/java/com/stocksScreener/service/UserService.java
+++ src/main/java/com/stocksScreener/service/UserService.java
@@ -3,6 +3,7 @@ package com.stocksScreener.service;
 import com.stocksScreener.model.User;
 import com.stocksScreener.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Service;
 
 import java.util.LinkedHashSet;
@@ -12,10 +13,12 @@ import java.util.Optional;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final StockSymbolService stockSymbolService;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, StockSymbolService stockSymbolService) {
         this.userRepository = userRepository;
+        this.stockSymbolService = stockSymbolService;
     }
 
     public Optional<User> findByUserEmail(String email) {
@@ -30,6 +33,7 @@ public class UserService {
         userRepository.save(user);
     }
 
+    @CacheEvict(value = "popularSymbols", allEntries = true)
     public void addToWatchList(String userEmail, List<String> symbols) {
         User user = findByUserEmail(userEmail).orElse(null);
         if (user != null) {
@@ -41,9 +45,12 @@ public class UserService {
             currentWatchlist.addAll(symbols);
             user.setWatchlist(currentWatchlist);
             saveUser(user);
+            for(String symbol: symbols)
+                stockSymbolService.updateSymbolPopularity(symbol, true);
         }
     }
 
+    @CacheEvict(value = "popularSymbols", allEntries = true)
     public void removeFromWatchlist(String userEmail, List<String> symbols) {
         User user = findByUserEmail(userEmail).orElse(null);
         if (user != null) {
@@ -51,6 +58,8 @@ public class UserService {
             currentWatchlist.removeIf(symbols::contains);
             user.setWatchlist(currentWatchlist);
             saveUser(user);
+            for(String symbol: symbols)
+                stockSymbolService.updateSymbolPopularity(symbol, false);
         }
     }
 }
